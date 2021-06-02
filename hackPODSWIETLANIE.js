6a35acaa5868012fec5e6e097a34cbf7af178b1e
Nubet <77124888+Nubet@users.noreply.github.com>
2021-06-03 00:00:49 +0200

    Update hackPODSWIETLANIE.js

--- hackPODSWIETLANIE.js
+++ hackPODSWIETLANIE.js
@@ -1,142 +1,114 @@
 if (wishaslo.indexOf("A", 0) >= 0)
 {
-//javascript:wislitera(0, "A")
 document.getElementById("wisliteraprzycisk0").style.backgroundColor = "purple";
 }
 
 if (wishaslo.indexOf("Ą", 0) >= 0)
 {
-//javascript:wislitera(1, "Ą")
 document.getElementById("wisliteraprzycisk1").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("B", 0) >= 0)
 {
-//javascript:wislitera(2, "B")
 document.getElementById("wisliteraprzycisk2").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("C", 0) >= 0)
 {
-//javascript:wislitera(3, "C")
 document.getElementById("wisliteraprzycisk3").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ć", 0) >= 0)
 {
-//javascript:wislitera(4, "Ć")
 document.getElementById("wisliteraprzycisk4").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("D", 0) >= 0)
 {
-//javascript:wislitera(5, "D")
 document.getElementById("wisliteraprzycisk5").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("E", 0) >= 0)
 {
-//javascript:wislitera(6, "E")
 document.getElementById("wisliteraprzycisk6").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ę", 0) >= 0)
 {
-//javascript:wislitera(7, "Ę")
 document.getElementById("wisliteraprzycisk7").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("F", 0) >= 0)
 {
-//javascript:wislitera(8, "F")
 document.getElementById("wisliteraprzycisk8").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("G", 0) >= 0)
 {
-//javascript:wislitera(9, "G")
 document.getElementById("wisliteraprzycisk9").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("H", 0) >= 0)
 {
-//javascript:wislitera(10, "H")
 document.getElementById("wisliteraprzycisk10").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("I", 0) >= 0)
 {
-//javascript:wislitera(11, "I")
 document.getElementById("wisliteraprzycisk11").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("J", 0) >= 0)
 {
-//javascript:wislitera(12, "J")
 document.getElementById("wisliteraprzycisk12").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("K", 0) >= 0)
 {
-//javascript:wislitera(13, "K")
 document.getElementById("wisliteraprzycisk13").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("L", 0) >= 0)
 {
-//javascript:wislitera(14, "L")
 document.getElementById("wisliteraprzycisk14").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ł", 0) >= 0)
 {
-//javascript:wislitera(15, "Ł")
 document.getElementById("wisliteraprzycisk15").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("M", 0) >= 0)
 {
-//javascript:wislitera(16, "M")
 document.getElementById("wisliteraprzycisk16").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("N", 0) >= 0)
 {
-//javascript:wislitera(17, "N")
 document.getElementById("wisliteraprzycisk17").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ń", 0) >= 0)
 {
-//javascript:wislitera(18, "Ń")
 document.getElementById("wisliteraprzycisk18").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("O", 0) >= 0)
 {
-//javascript:wislitera(19, "O")
 document.getElementById("wisliteraprzycisk19").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ó", 0) >= 0)
 {
-//javascript:wislitera(20, "Ó")
 document.getElementById("wisliteraprzycisk20").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("P", 0) >= 0)
 {
-//javascript:wislitera(21, "P")
 document.getElementById("wisliteraprzycisk21").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Q", 0) >= 0)
 {
-//javascript:wislitera(22, "Q")
 document.getElementById("wisliteraprzycisk22").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("R", 0) >= 0)
 {
-//javascript:wislitera(23, "R")
 document.getElementById("wisliteraprzycisk23").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("S", 0) >= 0)
 {
-//javascript:wislitera(24, "S")
 document.getElementById("wisliteraprzycisk24").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ś", 0) >= 0)
 {
-//javascript:wislitera(25, "Ś")
 document.getElementById("wisliteraprzycisk25").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("T", 0) >= 0)
 {
-//javascript:wislitera(26, "T")
 document.getElementById("wisliteraprzycisk26").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("U", 0) >= 0)
 {
-//javascript:wislitera(27, "U")
 document.getElementById("wisliteraprzycisk27").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("V", 0) >= 0)
@@ -146,31 +118,25 @@ document.getElementById("wisliteraprzycisk28").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("W", 0) >= 0)
 {
-//javascript:wislitera(29, "W")
 document.getElementById("wisliteraprzycisk29").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("X", 0) >= 0)
 {
-//javascript:wislitera(30, "X")
 document.getElementById("wisliteraprzycisk30").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Y", 0) >= 0)
 {
-//javascript:wislitera(31, "Y")
 document.getElementById("wisliteraprzycisk31").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Z", 0) >= 0)
 {
-//javascript:wislitera(32, "Z")
 document.getElementById("wisliteraprzycisk32").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ź", 0) >= 0)
 {
-//javascript:wislitera(33, "Ź")
 document.getElementById("wisliteraprzycisk33").style.backgroundColor = "purple";
 }
 if (wishaslo.indexOf("Ż", 0) >= 0)
 {
-//javascript:wislitera(34, "Ż")
 document.getElementById("wisliteraprzycisk34").style.backgroundColor = "purple";
 }
